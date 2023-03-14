6de306d0f1c3f9e90d1bb4e387db0bc587c4c6fe
jeongwoo-hong <ghdepddn@snu.ac.kr>
2023-03-14 19:36:34 +0900

    23.3.14.3

--- Yonsei/AI Math/functions.py
+++ Yonsei/AI Math/functions.py
@@ -34,3 +34,40 @@ salaries_guess = year2salary_unknown(years, basis_guess, raise_guess)
 print(salaries_guess)
 
 error = salaries_guess - salaries
+squared_error = error**2
+print(error)
+print(squared_error)
+print(squared_error.sum())
+
+def year2salary_unknown_basis(year, basis):
+    raise_per_year = 2
+    salary = basis + year * raise_per_year
+    return salary
+
+basis_guess = 1
+salaries_guess = year2salary_unknown_basis
+error = salaries_guess - salaries
+squared_error = error**2
+print(squared_error.sum())
+
+def compute_error(guess, gt):
+    error = guess - gt
+    squared_error = error**2
+    return squared_error.sum()
+
+basis_guess = 1
+salaries_guess = year2salary_unknown_basis(years, basis_guess)
+error = compute_error(salaries_guess, salaries)
+print(error)
+
+errors = []
+basis_guesses = np.arrange(-3, 3)
+for basis_guess in basis_guesses:
+    salaries_guess - year2salary_unknown_basis(years, basis_guess)
+    error = compute_error(salaries_guess, salaries)
+    errors.append(error)
+print(basis_guesses)
+print(errors)
+
+plt.plot(basis_guesses, errors)
+plt.show()
