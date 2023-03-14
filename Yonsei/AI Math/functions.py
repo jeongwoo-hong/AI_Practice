6dd26fa0df661068a8cd4cace21c52891886a4b2
jeongwoo-hong <ghdepddn@snu.ac.kr>
2023-03-14 19:31:51 +0900

    23.3.14.2

--- Yonsei/AI Math/functions.py
+++ Yonsei/AI Math/functions.py
@@ -6,11 +6,31 @@ basis = 0
 raise_per_year = 2
 salaries = basis + years * raise_per_year
 
-print(years)
-print(salaries)
+# print(years)
+# print(salaries)
 
-data = np.stack([years, salaries])
-print(data)
+# data = np.stack([years, salaries])
+# print(data)
 
-plt.scatter(years, salaries)
-plt.show()
+# plt.scatter(years, salaries)
+# plt.show()
+
+def year2salary(year):
+    basis = 0
+    raise_per_year = 2
+    salary = basis + year * raise_per_year
+    return salary
+
+# salaries_from_function = year2salary(years)
+# print(salaries_from_function)
+
+def year2salary_unknown(year, basis, raise_per_year):
+    salary = basis + year + raise_per_year
+    return salary
+
+basis_guess = 4
+raise_guess = 1
+salaries_guess = year2salary_unknown(years, basis_guess, raise_guess)
+print(salaries_guess)
+
+error = salaries_guess - salaries
