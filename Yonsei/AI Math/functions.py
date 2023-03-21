62ecda998431f8151db613f5706ca0becc7464d7
jeongwoo-hong <ghdepddn@snu.ac.kr>
2023-03-21 20:21:20 +0900

    23.3.21.1

--- Yonsei/AI Math/functions.py
+++ Yonsei/AI Math/functions.py
@@ -152,3 +152,33 @@ def composite(a_hat):
 
 # print(derivative(composite, 1))
 
+
+
+# gradient descent
+
+def sum_of_squares(xs):
+    return sum([x * x for x in xs])
+
+def gradient(f, xs, h = 1e-6):
+    derivs = []
+    for i in range(len(xs)):
+        xs_ -= np.copy(xs)
+        xs_[i] += h
+        fx2 = f(xs_)
+        xs_[i] -= 2*h
+        fx1 = f(xs_)
+        derivs.append((fx2 - fx1) / (2*h))
+    return np.array(derivs)
+
+    
+
+x0 = np.random.randn(2) * 3
+xi = x0
+step_size = 1e-1
+xis = [x0]
+for i in range(50):
+    g = gradient(sum_of_squares, xi)
+    print(xi, g)
+    xi = xi - g * step_size
+    xis.append(xi)
+print(xi)
