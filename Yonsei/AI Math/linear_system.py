df2c5852ea5426d1a627a398ac2fbd50ce9f6f57
jeongwoo-hong <ghdepddn@snu.ac.kr>
2023-04-04 19:46:58 +0900

    23.4.4.3

--- Yonsei/AI Math/linear_system.py
+++ Yonsei/AI Math/linear_system.py
@@ -9,14 +9,28 @@ x, y = np.meshgrid(np.arange(-5, 6), np.arange(-5, 6))
 # plt.grid()
 # plt.show()
 
-M = np.array([[3, 1],
-                [0, 2]])
+# ----------------------------------
 
-print(M)
-print(M.shape)
+# M = np.array([[3, 1],
+#                 [0, 2]])
+
+# print(M)
+# print(M.shape)
+
+# t_x, t_y = np.dot(M, np.array([x.flatten(), y.flatten()]))
+# plt.scatter(t_x, t_y)
+# plt.scatter(0, 0, color='orange')
+# plt.grid()
+# plt.show()
+
+# ----------------------------------
+
+M = np.array([[1, 5],
+                [0,1 ]])
 
 t_x, t_y = np.dot(M, np.array([x.flatten(), y.flatten()]))
 plt.scatter(t_x, t_y)
 plt.scatter(0, 0, color='orange')
 plt.grid()
 plt.show()
+
