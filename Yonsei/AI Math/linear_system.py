bca7c30b692f480b0bbae85ce0c7b096383766a9
jeongwoo-hong <ghdepddn@snu.ac.kr>
2023-04-04 19:42:18 +0900

    23.4.4.1

---
+++ Yonsei/AI Math/linear_system.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x, y = np.meshgrid(np.arange(-5, 6), np.arange(-5, 6))
+print(x) ; print(y)
+
+# plt.scatter(x, y)
+# plt.scatter(0, 0, color = 'orange')
+# plt.grid()
+# plt.show()
